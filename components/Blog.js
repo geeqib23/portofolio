8c48d3d6326a5c4fe41ce551c6993386c0851a23
geeqib23 <aqibsuhail23@gmail.com>
2021-06-18 10:13:15 +0530

    Minor changes and improvements

--- components/Blog.js
+++ components/Blog.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 function Blog({ posts }) {
-  console.log(posts)
+  // console.log(posts)
   const router = useRouter()
   return (
     <div className='container w-full h-full mx-auto my-5'>
       <p className='text-lg sm:text-xl'>
-        I am a tech-geek and read a lot online. In my blogs and tweets, I will
-        try to share with you the insights I have gained from my experiences.{' '}
+        I am a geek and read a lot online. In my blogs and tweets, I will try to
+        share with you the insights I have gained from my experiences :){' '}
       </p>
       <br></br>
       <div className='grid grid-cols-1 sm:grid-cols-2'>
@@ -23,7 +23,7 @@ function Blog({ posts }) {
                 <h2 className='mb-2 text-lg font-bold text-fontColor'>
                   {post.title}
                 </h2>
-                <p className='font-mono text-sm transition-all duration-200 text-secondary font-semibold '>
+                <p className='font-mono text-sm text-alternativeFont font-semibold '>
                   {post.date}
                 </p>
                 {/* <p className='font-mono text-sm font-light transition-all duration-200 text-secondaryText '>#web #react #node</p> */}
--- components/Navbar.js
+++ components/Navbar.js
@@ -16,14 +16,27 @@ function Navbar(props) {
       </Link> */}
       {/* <div className='block lg:hidden'>
         <button className='flex items-center px-3 py-2 text-gray-500 border border-gray-600 rounded appearance-none hover:text-gray-800 hover:border-teal-500 focus:outline-none'>
-          <svg className='w-3 h-3 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
-            <title>Menu</title>
-            <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
-          </svg>
+        <svg className='w-3 h-3 fill-current' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
+        <title>Menu</title>
+        <path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+        </svg>
         </button>
       </div> */}
       <div>
         <ul className='inline-flex'>
+          <li>
+            <Link href='/' passHref itemProp='url'>
+              <a
+                className={
+                  router.pathname == '/' ? 'navbarItems active' : 'navbarItems'
+                }
+                alt='KnowMe'
+                itemProp='url'
+              >
+                KnowMe
+              </a>
+            </Link>
+          </li>
           <li>
             <Link href='/projects' passHref itemProp='url'>
               <a
@@ -51,19 +64,6 @@ function Navbar(props) {
                 itemProp='url'
               >
                 Blogs
-              </a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/' passHref itemProp='url'>
-              <a
-                className={
-                  router.pathname == '/' ? 'navbarItems active' : 'navbarItems'
-                }
-                alt='KnowMe'
-                itemProp='url'
-              >
-                KnowMe
               </a>
             </Link>
           </li>
